60b8c5552daa9adbe0556f6ff2396b2c17105499
borislav nikolov <jack@brokensociety.org>
2012-08-27 00:31:17 +0300

    wip

---
+++ example/web.go
@@ -0,0 +1,22 @@
+package main
+import (
+	"github.com/jackdoe/session"
+	"github.com/jackdoe/godzilla"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+func h(ctx *godzilla.Context) {
+	ctx.Output["list"] = []interface{}{"a","b"}
+	ctx.Render("sample")
+}
+
+
+func main() {
+	db, _ := sql.Open("sqlite3", "./foo.db")
+	defer db.Close()
+	session.Init(db,"session")
+	session.CookieKey = "go.is.awesome"
+	session.CookieDomain = "localhost"
+	godzilla.Route("^/(sample)/(.*)$",h)	
+	godzilla.Start("localhost","8080",db)
+}
