f6e432996e9e52b6260204d07c1271c63fc7743c
borislav nikolov <jack@brokensociety.org>
2012-08-27 00:23:25 +0300

    initial

---
+++ godzilla.go
@@ -0,0 +1,78 @@
+package godzilla
+import (
+	"net/http"
+	"github.com/jackdoe/session"
+	"database/sql"
+	"log"
+	"strings"
+	"regexp"
+	"text/template"
+)
+type Context struct {
+	W http.ResponseWriter
+	R *http.Request
+	S *session.SessionObject
+	Db *sql.DB
+	Output map[string]interface{}
+	Layout string
+	Splat []string
+}
+var (
+	Views string = "./v/"
+	TemplateExt string = ".html"
+)
+func (this *Context) IsXHR() bool {
+	v,ok := this.R.Header["X-Requested-With"]; if ok {
+		for _,val := range v {
+			if strings.ToLower(val) == strings.ToLower("XMLHttpRequest") {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (this *Context) Render(name string) {
+	var ts *template.Template
+	var err error
+	gen := func(s string) string {
+		return Views + s + TemplateExt
+	}
+	if this.IsXHR() {
+		ts,err = template.ParseFiles(gen(name))
+		ts.Parse(`{{template "yield" .}}`)
+	} else {
+		ts, err = template.ParseFiles(gen(this.Layout),gen(name))
+	}
+	if err != nil {
+		log.Printf("error rendering: %s - %s",name,err.Error())
+	}
+	ts.Execute(this.W, this.Output)
+}
+
+var routes = map[*regexp.Regexp]func(*Context)(){}
+
+func Route(pattern string, handler func(*Context)()) {
+	routes[regexp.MustCompile(pattern)]=handler
+}
+
+func Start(host string, port string,db *sql.DB) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		s := session.New(w,r)
+		path := r.URL.Path
+		w.Header().Set("Content-Type", "text/html")
+		for k, v := range routes {
+			matched := k.FindStringSubmatch(path)
+			if matched != nil {
+				log.Printf("%s @ [%s]",path,k)
+				v(&Context{w,r,s,db,make(map[string]interface{}),"layout",matched})
+				return
+			}
+		}
+		log.Printf("%s - NOT FOUND",path)
+		http.NotFound(w,r)
+	})
+	log.Printf("started: http://%s:%s/",host,port)
+	log.Fatal(http.ListenAndServe(host + ":" + port, nil))
+}
+
