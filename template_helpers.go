d8d564d082327494bb20f2e32a1d7a4bec5bdf47
borislav nikolov <jack@brokensociety.org>
2012-08-29 23:03:17 +0300

    template_js: add / at the loading url

--- template_helpers.go
+++ template_helpers.go
@@ -6,13 +6,13 @@ import (
 
 //{{ js "calendar/cell" "calendar/row"}} will render
 
-//	var calendar_cell; $.get('calendar/cell.jst',function(data) { calendar_cell = data });
-//	var calendar_row; $.get('calendar/row.jst',function(data) { calendar_row = data });
+//	var calendar_cell; $.get('/calendar/cell.jst',function(data) { calendar_cell = data });
+//	var calendar_row; $.get('/calendar/row.jst',function(data) { calendar_row = data });
 func Template_js(args ...string) string {
 	s := ""
 	for _,v := range args {
 		us := strings.Replace(v,"/","_",-1)
-		s += fmt.Sprintf("var %s; $.get('%s.jst',function(data) { %s = data });",us,v,us)
+		s += fmt.Sprintf("var %s; $.get('/%s.jst',function(data) { %s = data });",us,v,us)
 	}
 	return s
 }
