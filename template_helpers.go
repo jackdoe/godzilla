8fc36e89a901cff6e33a9aa373255f2f5fdce6c5
borislav nikolov <jack@brokensociety.org>
2012-08-29 22:59:41 +0300

    update readme

--- template_helpers.go
+++ template_helpers.go
@@ -5,15 +5,15 @@ import (
 )
 
 //{{ js "calendar/cell" "calendar/row"}} will render
-//		<script type='text/template' id='calendar/cell' src='/calendar/cell.js'></script><script>var calendar_cell = $('#calendar/cell').html();</script>
-//		<script type='text/template' id='calendar/row' src='/calendar/row.js'></script><script>var calendar_row = $('#calendar/row').html();</script>
+
+//	var calendar_cell; $.get('calendar/cell.jst',function(data) { calendar_cell = data });
+//	var calendar_row; $.get('calendar/row.jst',function(data) { calendar_row = data });
+
 func Template_js(args ...string) string {
 	s := ""
 	for _,v := range args {
 		us := strings.Replace(v,"/","_",-1)
-		// s += fmt.Sprintf("<script type='text/template' id='%s'> src='/%s.jst'></script><script>var %s = $('#%s').load($('#%s').attr('src')).html();</script>",us,v,us,us,us)
 		s += fmt.Sprintf("var %s; $.get('%s.jst',function(data) { %s = data });",us,v,us)
-
 	}
 	return s
 }
