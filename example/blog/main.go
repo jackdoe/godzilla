4e3ce99f1afb7d653dc13d32e598cac1dcbadb62
borislav nikolov <jack@brokensociety.org>
2012-09-02 14:56:44 +0300

    cleanup blog example

--- example/blog/main.go
+++ example/blog/main.go
@@ -33,9 +33,9 @@ func list(ctx *godzilla.Context) {
 	ctx.O["link"] = link
 	_, ok := ctx.Params["category"]
 	if ok {
-		ctx.O["items"] = ctx.Query("SELECT a.*,(strftime('%s', 'now') - a.created_at) as ago FROM posts a, post_category b WHERE a.id = b.post_id AND b.category_id=? ORDER BY a.created_at DESC", ctx.Params["category"])
+		ctx.O["items"] = ctx.Query("SELECT a.*,(strftime('%s', 'now') - a.updated_at) as ago FROM posts a, post_category b WHERE a.id = b.post_id AND b.category_id=? ORDER BY a.updated_at DESC", ctx.Params["category"])
 	} else {
-		ctx.O["items"] = ctx.Query("SELECT *,(strftime('%s', 'now') - created_at) as ago FROM posts ORDER BY created_at DESC")
+		ctx.O["items"] = ctx.Query("SELECT *,(strftime('%s', 'now') - updated_at) as ago FROM posts ORDER BY updated_at DESC")
 	}
 	ctx.O["is_admin"] = is_admin(ctx)
 	ctx.Render()
@@ -53,19 +53,14 @@ func modify(ctx *godzilla.Context) {
 		return
 	}
 	var output interface{}
-	var j map[string]interface{}
-	e := json.Unmarshal([]byte(ctx.Sparams["json"]), &j)
-	if j == nil {
-		j = map[string]interface{}{}
-	}
 	switch ctx.R.Method {
-	case "PATCH":
-		output = ctx.Query("SELECT a.* FROM categories a, post_category b WHERE a.id = b.category_id AND b.post_id=?", object_id)
 	case "GET":
 		output = ctx.FindById(object_type, object_id)
 	case "POST":
-		if j["id"] == nil {
-			j["created_at"] = time.Now().Unix()
+		var j map[string]interface{}
+		json.Unmarshal([]byte(ctx.Sparams["json"]), &j)
+		if j == nil {
+			j = map[string]interface{}{}
 		}
 		j["updated_at"] = time.Now().Unix()
 		id, _ := ctx.Replace(object_type, j)
@@ -73,15 +68,8 @@ func modify(ctx *godzilla.Context) {
 	case "DELETE":
 		ctx.DeleteById(object_type, object_id)
 		output = "deleted " + object_id + "@" + object_type
-	case "OPTIONS":
-		output = ctx.Query("SELECT * FROM `" + object_type + "`") //; ORDER BY created_at,updated_at")
 	}
-	b, e := json.Marshal(output)
-	if e != nil {
-		b = []byte(e.Error())
-	}
-	ctx.ContentType(godzilla.TypeJSON)
-	ctx.W.Write(b)
+	ctx.RenderJSON(output,404)
 }
 func show(ctx *godzilla.Context) {
 	o := ctx.FindById("posts", ctx.Splat[1])
